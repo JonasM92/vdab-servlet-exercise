b76cad71da8cb9bbaa15719855bf117a18826b6f
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2015-06-24 11:23:30 +0200

    Moved OrderItem as inner class of ShoppingServlet to make exercise examples more isolated

--- src/main/java/be/vdab/ShoppingServlet.java
+++ src/main/java/be/vdab/ShoppingServlet.java
@@ -78,4 +78,32 @@ public class ShoppingServlet extends HttpServlet {
         writer.println("<div>Grand total: " + total + "</div>");
         writer.println("<a href='form.html'>More tea</a>");
     }
+
+    public static class OrderItem {
+        private String product;
+        private int quantity;
+        private double price;
+
+        public OrderItem(String product, int quantity, double price) {
+            this.product = product;
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        public String getProduct() {
+            return product;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public double getTotal() {
+            return price * quantity;
+        }
+    }
 }
